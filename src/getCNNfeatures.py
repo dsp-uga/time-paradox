94f8c4af14b02a5d7d54e1f053a5b92e9ef41701
Nihal Soans <nihalsoans91@gmail.com>
2018-04-18 13:37:29 -0400

    added few tweks

--- src/getCNNfeatures.py
+++ src/getCNNfeatures.py
@@ -1,8 +1,3 @@
-print("Importing OS related packages such as read write etc")
-# OS related such as read write etc
-import os
-import csv
-
 # math related
 print("Importing Math packages such as numpy")
 import numpy as np
@@ -11,39 +6,56 @@ np.set_printoptions(threshold=np.nan)
 
 # tensorflow related
 print("Importing tensorflow related packages")
+from urllib.request import urlretrieve
+from os.path import isfile, isdir
+from tqdm import tqdm
+
+vgg_dir = 'tensorflow_vgg/'
+# Make sure vgg exists
+if not isdir(vgg_dir):
+    raise Exception("VGG directory doesn't exist!")
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+if not isfile(vgg_dir + "vgg16.npy"):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='VGG16 Parameters') as pbar:
+        urlretrieve(
+            'https://s3.amazonaws.com/content.udacity-data.com/nd101/vgg16.npy',
+            vgg_dir + 'vgg16.npy',
+            pbar.hook)
+else:
+    print("Parameter file already exists!")
+
 import tensorflow as tf
 from tensorflow_vgg import vgg16
 from tensorflow_vgg import utils
-
-# matlab packages
-print("Importing Matlab Packages")
-import matplotlib.pyplot as plt
-
-# Scikit Packages
-print("Importing Scikit Packages")
-from scipy.ndimage import imread
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import StratifiedShuffleSplit
 
 
 class Find_hash(object):
     def __init__(self):
         # The graph ready
+
         print("Reconstructing Network")
         self.input_ = tf.placeholder(tf.float32, [None, 224, 224, 3])
         self.vgg = vgg16.Vgg16()
         self.vgg.build(self.input_)
 
-    def detect_hash(self, path):
+    def detect_hash(self, path,sess):
         self.img = utils.load_image(path)
         self.img = self.img.reshape((1, 224, 224, 3))
 
-        print("Running Getting Hash for "+path)
-        with tf.Session() as sess:
-            self.feed_dict = {self.input_: self.img}
-            self.code = sess.run(self.vgg.relu6, feed_dict=self.feed_dict)
+        print("Running Getting Hash for " + path)
 
-        return (self.code[0])
+        self.feed_dict = {self.input_: self.img}
+        self.hash = sess.run(self.vgg.relu6, feed_dict=self.feed_dict)
+
+        return (self.hash[0])
 
 
 hash = Find_hash()
@@ -52,30 +64,9 @@ import os
 
 path = "data"
 index = {}
-op = open("index.json", "w")    # writing hashes to json file
-op.write("[\n")
-
-imgs = os.listdir(path)         # list of images
-imcount = len(imgs)             # no. of images
-for i in range(imcount):        # converting to json format {id: _, hash: _}
-    _id = imgs[i][:len(imgs[i]) - 4]    # image id
-    _hash = hash.detect_hash(os.path.join(path, imgs[i]))   # vgg16 hash
-    _hash = _hash.tolist()
-    index[_id] = _hash
-
-    # writing to json
-    op.write('{\n')
-    op.write('\t"id": "%s",\n' % _id)
-    op.write('\t"hash": [')
-
-    for h in _hash[:-1]:
-        op.write('%.3f,' % h)
-    op.write('%.3f]\n' % _hash[-1]) # last element of hash (should avoid ',')
-
-    if i != imcount - 1:
-        op.write('},\n')
-    else:
-        op.write('}\n')         # last entry to json (should avoid ',')
-
-op.write("]")
-op.close()
+with open("index", 'w') as f:
+    with tf.Session() as sess:
+        for i in os.listdir(path):
+            x = hash.detect_hash(os.path.join("data", i),sess)
+            index[i[:len(i) - 3]] = x
+            f.write('%s:%s\n' % (i[:len(i) - 3], x))
