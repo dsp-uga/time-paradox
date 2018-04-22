ecd6f2a4bf024d14fa3a96362f3fd5647213839e
Nihal Soans <nihalsoans91@gmail.com>
2018-04-21 20:02:31 -0400

    Added arg skelton

--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,19 @@
 
+import argparse
+import sys
+import os
+# This method will activate the argument parser and will have all the paramenters that will  be pased through the code
+def get_args(args):
+    parser = argparse.ArgumentParser(description='Blah Blah')
+    parser.add_argument('Mandatory', help='Help is on the way', type=str)
+    parser.add_argument('--optional', help='Its still on he way', default="wahtever",type=str)
+    
+    return parser.parse_args(args)
+
+
+
+def main(args=None):
+    print ("Cilia")
+    if args is None:
+        args = sys.argv[1:]
+    args = get_args(args)
