f98faab0763b721a9c6facaaaf3b7b4ae9b8be7f
Vamsi Nadella <nadellavamsi94@gmail.com>
2018-04-26 00:22:27 -0400

    Hasing using vgg-16

--- src/pre_processing/CNNfeatures.py
+++ src/pre_processing/CNNfeatures.py
@@ -1,7 +1,7 @@
 print("Importing OS related packages such as read write etc")
 # OS related such as read write etc
 import os
-import csv
+
 
 # math related
 print("Importing Math packages such as numpy")
@@ -26,7 +26,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import StratifiedShuffleSplit
 
 
-class Find_hash(object):
+class CnnHash(object):
     def __init__(self):
         # The graph ready
         print("Reconstructing Network")
@@ -38,7 +38,7 @@ class Find_hash(object):
         self.img = utils.load_image(path)
         self.img = self.img.reshape((1, 224, 224, 3))
 
-        print("Getting Hash for "+path)
+#        print("Getting Hash for "+path)
 
         self.feed_dict = {self.input_: self.img}
         self.code = sess.run(self.vgg.relu6, feed_dict=self.feed_dict)
@@ -46,37 +46,3 @@ class Find_hash(object):
         return (self.code[0])
 
 
-hash = Find_hash()
-
-import os
-
-path = "data"
-index = {}
-op = open("index.json", "w")    # writing hashes to json file
-op.write("[\n")
-
-imgs = os.listdir(path)         # list of images
-imcount = len(imgs)             # no. of images
-with tf.Session() as sess:
-    for i in range(imcount):        # converting to json format {id: _, hash: _}
-        _id = imgs[i][:len(imgs[i]) - 4]    # image id
-        _hash = hash.detect_hash(os.path.join(path, imgs[i]),sess)   # vgg16 hash
-        _hash = _hash.tolist()
-        index[_id] = _hash
-
-        # writing to json
-        op.write('{\n')
-        op.write('\t"id": "%s",\n' % _id)
-        op.write('\t"hash": [')
-
-        for h in _hash[:-1]:
-            op.write('%.4f,' % h)
-        op.write('%.4f]\n' % _hash[-1]) # last element of hash (should avoid ',')
-
-        if i != imcount - 1:
-            op.write('},\n')
-        else:
-            op.write('}\n')         # last entry to json (should avoid ',')
-
-op.write("]")
-op.close()
