c6fa99857a2eb85fb6982e7c51fe14b50437f661
Vinay Kumar <vinay.bingi@gmail.com>
2018-04-27 15:19:10 -0400

    updated with formatting output csv

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 from clustering.kmeanscluster import Kmeans
 from pre_processing.dataIO import Data
+from post_prediction.createOutputCSV import FormatCsv
 import numpy as np
 import pandas as pd
 pd.options.display.max_colwidth
@@ -9,10 +10,11 @@ test_json_path = sys.argv[1]
 clusters_path = sys.argv[2] 
 
 data = Data()
+formatter = FormatCsv()
 json_test, test_hashes = data.loadJson(test_json_path)
 kmeans = Kmeans(json_hash=json_data,hashs=hashes)
 clusters = kmeans.load(clusters_path)
-csvfile = open('submission.csv','wb')
+csvfile = open('prediction.csv','wb')
 for i in range(len(test_hashes)):
     imgid = json_test[i]['id']
     clstrid = kmeans.predict(clusters,np.array([test_hashes[i]]))
@@ -23,6 +25,7 @@ for i in range(len(test_hashes)):
     csvfile.write(line)
     csvfile.write('\n')
 csvfile.close()
+formatter.formatcsv('test.csv', 'prediction.csv', 'submission.csv')
 print("Submission csv ready")
 
 
